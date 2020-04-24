bdaafc388de6ce3e4d3f348207022ba3b54fe73f
Pedro Henrique <phjfarias@hotmail.com>
2020-04-23 23:54:32 -0300

    add ex1

---
+++ ex1/src/main/java/school/cesar/ppcd/av1/ex1/server/SharedCounterServerRunnable.java
@@ -0,0 +1,50 @@
+package school.cesar.ppcd.av1.ex1.server;
+
+import java.net.Socket;
+import java.io.*;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
+
+public class SharedCounterServerRunnable implements Runnable {
+	private Socket client;
+	private int counter = 0;
+	private DataInputStream inputStream;
+	private DataOutputStream outputStream;
+	private Semaphore mutex = new Semaphore(1);
+
+	public SharedCounterServerRunnable(Socket client) throws IOException {
+		this.client = client;
+		this.inputStream = new DataInputStream(client.getInputStream());
+		this.outputStream = new DataOutputStream(client.getOutputStream());
+	}
+
+	public void incrementCounter() {
+		this.counter++;
+	}
+
+	public void run() {
+		// TODO Auto-generated method stub
+
+		try {
+			while (true) {
+				if (inputStream.read() == 73) {
+					try {
+						mutex.acquire();
+						this.incrementCounter();
+						this.outputStream.writeInt(this.counter);
+						TimeUnit.SECONDS.sleep(1);
+					} finally {
+						mutex.release();
+					}
+				}
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
