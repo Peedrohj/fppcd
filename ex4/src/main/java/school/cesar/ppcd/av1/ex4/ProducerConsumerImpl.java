33f6257d4848b2e96ccffdf0f0bb9ca25727c38d
Pedro Henrique <phjfarias@hotmail.com>
2020-04-24 23:15:46 -0300

    add ex4

--- ex4/src/main/java/school/cesar/ppcd/av1/ex4/ProducerConsumerImpl.java
+++ ex4/src/main/java/school/cesar/ppcd/av1/ex4/ProducerConsumerImpl.java
@@ -1,15 +1,43 @@
 package school.cesar.ppcd.av1.ex4;
 
-public class ProducerConsumerImpl implements ProducerConsumer {
+import java.util.ArrayList;
+import java.util.List;
 
-	public void produce(SomeRequest request) {
-		// TODO Auto-generated method stub
-		
+public class ProducerConsumerImpl implements ProducerConsumer {
+	private final List<SomeRequest> list = new ArrayList();
+	private final int size = 2;
+
+	public void produce(final SomeRequest request) {
+		synchronized (this) {
+			if (this.list.size() == size) {
+				try {
+					wait();
+				} catch (final InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			} else {
+				this.list.add(request);
+				notify();
+			}
+		}
 	}
 
 	public SomeRequest consume() {
-		// TODO Auto-generated method stub
-		return null;
+		synchronized (this) {
+			if (this.list.size() == 0) {
+				try {
+					wait();
+				} catch (final InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+
+			notify();
+			return this.list.remove(0);
+		}
+
 	}
 
 }
